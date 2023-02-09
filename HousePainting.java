bd4f799c89de671908ce7d03107062b6204beba7
Ryan Basile <91210643+s1324902@users.noreply.github.com>
2023-02-09 10:35:07 -0500

    Add files via upload

---
+++ HousePainting.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class HousePainting {
+
+	public static void main(String[] args) {
+		Scanner scnr = new Scanner(System.in);
+		
+		double costSquareFt, Houselength, Housewidth, Househeight, numWindows, Windowheight, Windowlength, numDoor, doorHeight, doorLength, side, sidePeak, windowSquareFt;
+		System.out.println("Please enter the length, width and the height of the house");
+		lenth = in.nextDouble();
+
+	}
+
+}
