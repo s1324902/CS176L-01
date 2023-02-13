ac239141961a8f543a01b308a8addfaf05c7b7fb
Ryan Basile <91210643+s1324902@users.noreply.github.com>
2023-02-13 15:58:13 -0500

    Add files via upload

---
+++ MicrowaveTester.java
@@ -0,0 +1,17 @@
+
+public class MicrowaveTester {
+
+	public static void main(String[] args) {
+		Microwave myMicrowave = new Microwave();
+		
+		myMicrowave.Time();
+		myMicrowave.Time();
+		myMicrowave.PowerSwitch();
+		myMicrowave.start();
+		myMicrowave.reset();
+		
+	
+	
+	}
+
+}
